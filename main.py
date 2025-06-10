746072bfe2290b26baf201c8fa1406e5aaf746fa
dbs-tjd50 <sn2025183@shs.hs.kr>
2025-06-10 14:42:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.title("첫 app")
 st.write("hello")
+name=st.text_input("이름 입력")
+if name:
+  if name=="정윤성":
+    st.success("반갑습니다. 홍길동님!")
+  else:
+    st.warning("누구세요?)
